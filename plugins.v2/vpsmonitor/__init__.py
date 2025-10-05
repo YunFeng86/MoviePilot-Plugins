b6a6b285915b0a8ef817e00cc120fb616db78e87
YunFeng <YunFengZero@outlook.com>
2025-10-06 00:51:39 +0800

    fix: 修复插件版本号，调整授权组件布局

--- plugins.v2/vpsmonitor/__init__.py
+++ plugins.v2/vpsmonitor/__init__.py
@@ -25,7 +25,7 @@ class VPSMonitor(_PluginBase):
     plugin_name = "Netcup VPS 限速监控"
     plugin_desc = "定时检测NC SCP 下 VPS 是否被限速，并通过通知插件发送结果。"
     plugin_icon = "https://raw.githubusercontent.com/YunFeng86/MoviePilot-Plugins/main/icons/OneBot_A.png"
-    plugin_version = "0.2.1"
+    plugin_version = "0.2.0"
     plugin_author = "YunFeng"
     author_url = "https://github.com/YunFeng86"
     plugin_config_prefix = "vpsmonitor_"
